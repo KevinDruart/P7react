59124c4ba9f57d1bbbba8a40e6d6c636306b0807
Druart <65339848+KevinDruart@users.noreply.github.com>
2021-09-25 00:18:21 +0200

    maj app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 
-import './App.css';
+import './App.scss';
+import Footer from './components/footer/footer';
+import Sign from './components/sign/sign';
 
 function App() {
   return (
     <div className="App">
+      <Sign />
+      <Footer />
+
 
     </div>
   );
