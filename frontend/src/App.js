9c5f10551bfdce81b043ddb811f081d322e25001
KevinDruart <65339848+KevinDruart@users.noreply.github.com>
2021-10-20 11:16:59 +0200

    maj authentification debut

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, } from 'react-router-dom'
 import Index from './pages/Index/Index';
 import Error404 from './pages/erreur404/Error404';
 
@@ -13,24 +13,48 @@ import Login from './pages/login/Login';
 
 
 function App() {
-  return (
-    <div className="App">
 
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route path="/login" exact component={Login} />
-          <Route path="/signup" exact component={Signup} />
-          <Route path="/" exact component={Index} />
-          <Route path="/home" exact component={Home} />
-          <Route component={Error404} />
-        </Switch>
-      </BrowserRouter>
-      <Footer />
+  const user = JSON.parse(localStorage.getItem("user")) ;
+  const token =  user.token;
 
 
-    </div>
-  );
+  if (token === null || token === undefined) {
+    return (
+      <>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Index} />
+            <Route path="/signup" exact component={Signup} />
+            <Route path="/login" exact component={Login} />
+            <Route component={Error404} />
+          </Switch>
+
+        </BrowserRouter>
+      </>
+    );
+  } else {
+
+    return (
+      <div className="App">
+
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route path="/login" exact component={Login} />
+            <Route path="/signup" exact component={Signup} />
+            <Route path="/" exact component={Index} />
+            <Route path="/home" exact component={Home} />
+            <Route component={Error404} />
+          </Switch>
+
+        </BrowserRouter>
+        <Footer />
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
