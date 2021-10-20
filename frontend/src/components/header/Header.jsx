ee8dba29f5b0924845e736a451f6b2fde3ddcaca
KevinDruart <65339848+KevinDruart@users.noreply.github.com>
2021-10-20 11:57:00 +0200

    maj navbar

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import Classes from './header.module.css';
 import NavBar from './Navigation/NavBar';
 
 const Header = () => {
+
     return (
         <header className={["border-bottom no-gutters fixed-top", Classes.header].join(' ')}>
             <div className={["container", Classes.containerHeader].join(' ')}>
