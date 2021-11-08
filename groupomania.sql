28bdc1796508fd499c987df3846d2c68927685c6
KevinDruart <65339848+KevinDruart@users.noreply.github.com>
2021-11-08 15:29:23 +0100

    maj bdd

--- groupomania.sql
+++ groupomania.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3307
--- Generation Time: Nov 03, 2021 at 08:45 AM
+-- Generation Time: Nov 08, 2021 at 02:28 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
 
@@ -39,6 +39,19 @@ CREATE TABLE `comment` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `post_id` int(11) UNSIGNED NOT NULL,
+  `like` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `post`
 --
 
@@ -46,8 +59,8 @@ CREATE TABLE `post` (
   `id` int(10) UNSIGNED NOT NULL,
   `title` varchar(30) NOT NULL,
   `user_id` int(11) UNSIGNED NOT NULL,
-  `content` text NOT NULL,
-  `image` varchar(250) NOT NULL,
+  `content` text,
+  `image` varchar(250) DEFAULT NULL,
   `time_post` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -57,23 +70,8 @@ CREATE TABLE `post` (
 
 INSERT INTO `post` (`id`, `title`, `user_id`, `content`, `image`, `time_post`) VALUES
 (1, 'test', 42, 'hello je test ', './images/groupomania.png', '2021-09-16 14:36:13'),
-(2, 'New test', 9, 'Je poste un nouveau test ', './upload/plage.jpg', '2021-10-13 16:13:20'),
-(3, 'test profile post', 49, 'voici un test pour essayer d\'afficher les post du membre en question sur son profile', './upload/tour-eiffel.jpg', '2021-10-22 17:37:16'),
-(4, 'tourcoing', 49, 'voici tourcoing', './upload/plage.jpg', '2021-10-25 08:13:20'),
-(5, 'tourcoing', 49, 'voici tourcoing', 'http://localhost:3000/images/tourcoing_1635337654561.png', '2021-10-27 14:27:34'),
-(6, 'new test', 49, 'yoyoyo', 'http://localhost:3000/images/tourcoing_1635339235356.png', '2021-10-27 14:53:55'),
 (7, 'test', 49, 'test', 'http://localhost:3000/images/tourcoing_1635339673899.png', '2021-10-27 15:01:13'),
-(8, 'test', 49, 'test3', 'http://localhost:3000/images/tourcoing_1635339777383.png', '2021-10-27 15:02:57'),
-(9, 'new test', 49, 'test2', 'http://localhost:3000/images/tourcoing_1635363064082.png', '2021-10-27 21:31:04'),
-(10, 'tourcoing', 49, 'voici tourcoing', 'http://localhost:3000/images/tourcoing_1635373403731.png', '2021-10-28 00:23:23'),
-(11, 'testsoir', 49, 'test4', 'http://localhost:3000/images/erreur_addpost2_1635373643352.jpg', '2021-10-28 00:27:23'),
-(12, 'kevin', 49, 'test2', 'http://localhost:3000/images/erreur_addpost2_1635373692750.jpg', '2021-10-28 00:28:12'),
-(13, 'new test', 49, 'yoyoyo', 'http://localhost:3000/images/erreur_addpost1_1635373718265.jpg', '2021-10-28 00:28:38'),
-(14, 'test', 49, 'est le roi', 'http://localhost:3000/images/addpostproblem3_1635426008205.jpg', '2021-10-28 15:00:08'),
-(15, 'test', 49, 'test', 'http://localhost:3000/images/addpostproblem2_1635428024736.jpg', '2021-10-28 15:33:44'),
-(17, '', 49, '', 'http://localhost:3000/images/tourcoing_1635766499670.png', '2021-11-01 12:34:59'),
-(18, '', 49, '', 'http://localhost:3000/images/tourcoing_1635766650417.png', '2021-11-01 12:37:30'),
-(19, '', 49, '', 'http://localhost:3000/images/index_1635928606536.jpg', '2021-11-03 09:36:46');
+(8, 'new test', 55, 'test', 'http://localhost:3000/images/tourcoing_1636313742200.png', '2021-11-07 20:35:42');
 
 -- --------------------------------------------------------
 
@@ -97,18 +95,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `firstname`, `emailMasked`, `email`, `password`, `dateSignup`, `roles`) VALUES
-(4, 'teste', 'kevin', 'ke***@***********.fr', 'kevin@groupomania.fr', '$2b$10$TbNoqdyVm1DovCushIbkROeyWxZ3EFfLiJsiiCTZCr0CUCZqkoI3a', '2021-08-31 12:54:04', 'member'),
-(5, 'toto', 'titi', 'to**@***********.fr', 'toto@groupomania.fr', '$2b$10$BQfjFrDTuYZTZsq4KKccDeEeLMDqNx/kpLMTOWvZOhXlaUqXd.0Hy', '2021-08-31 12:54:04', 'member'),
-(6, 'toto', 'titi', 'to**@***********.fr', 'toto@groupomania.fr', '$2b$10$l09oa59fq0QPRHQ8HokuaufbbNZ6K7FJZ5iEfEi1Je9EUnSkbUw9.', '2021-08-31 12:54:04', 'member'),
-(7, 'toto', 'titi', 'to**@***********.fr', 'toto@groupomania.fr', '$2b$10$jQSrPcA/90QFyANVpWWTX.60bgqAG4Hmth4LnHnBRrfq6fY/z.aFm', '2021-08-31 12:54:04', 'member'),
-(8, 'toto', 'titi', 'to**@***********.fr', 'toto@groupomania.fr', '$2b$10$kp9c5vy3ElW92jKpHE6Rdu0d81AU/zNYOhCACri63ugYSiYSlaigC', '2021-08-31 12:54:04', 'member'),
-(9, 'toto', 'titi', 'to**@***********.fr', 'toto@groupomania.fr', '$2b$10$oYkbaZa7mDdShSeyCTuIZusMs4zQT5HphZu7DOEPtOfT4L0/Nb3YW', '2021-08-31 12:55:27', 'member'),
-(10, 'toto', 'titi', 'to**@***********.fr', 'toto@groupomania.fr', '$2b$10$MmI7r6NKKsoN5OWuG0xsXOeue48Ozd7ibgc8CqYQYBLeYzJOKg0b.', '2021-08-31 14:10:16', 'member'),
-(11, 'toto', 'titi', 'to**@***********.fr', 'toto@groupomania.fr', '$2b$10$jprpWn7JevDazCf88Nw8oucAGpjKpKSVuA/ozaLa05qBz8tpfwcxW', '2021-08-31 14:10:21', 'member'),
-(12, 'toto', 'titi', 'to**@***********.fr', 'toto@groupomania.fr', '$2b$10$hNwK8XKcW/92nrMFoRhUEObL263TYIforrxOKkbYhx1Mxucvppgua', '2021-08-31 14:28:36', 'member'),
 (13, 'toto', 'titi', 'to**@***********.fr', 'toto@groupomania.fr', '$2b$10$T9C5biLEEJfla4b0OJ7KTucEfGUALFzesS.PYtIRV.y1WOGBygOZG', '2021-08-31 14:49:02', 'member'),
-(14, 'toto', 'titi', 'to**@***********.fr', 'toto@groupomania.fr', '$2b$10$qolxAR.JZA6iKMTdb4G10OXNGrOGcuttUMD1lEA1pOJeA9xS6YyUC', '2021-08-31 16:10:43', 'member'),
-(15, 'toto', 'titi', 'to**@***********.fr', 'toto@groupomania.fr', '$2b$10$z.H4VmTSQU14zv36fPF.e.FrI.6HWxqL4suNb/3UWbQ3SGjZMmmqW', '2021-08-31 16:10:45', 'member'),
 (16, 'toto', 'titi', 'to**@***********.fr', 'toto@groupomania.fr', '$2b$10$lzUHaIaMLUwuTzBskJcYRu/39azHWQm1k7Q4olijo.9lAwgpTQnde', '2021-08-31 16:12:05', 'member'),
 (17, 'toto', 'titi', 'to**@***********.fr', 'toto@groupomania.fr', '$2b$10$I34rDS3TXvD4UPP156xbVOxcqkx/W.gAxo4imrHpHrBKS8Zu.Wf4O', '2021-08-31 16:35:57', 'member'),
 (18, 'toto', 'titi', 'ti**@***********.fr', 'titi@groupomania.fr', '$2b$10$79zVVpZdy8ycV8ap9obpdOchksAOnk3JNcRAjzvRmcDhpYd1Zp9uO', '2021-08-31 17:04:30', 'member'),
@@ -144,7 +131,10 @@ INSERT INTO `user` (`id`, `name`, `firstname`, `emailMasked`, `email`, `password
 (48, 'Druart', 'Kevin', 'ta**@***********.fr', 'tato@groupomania.fr', '$2b$10$HFgzn7rHLz0p/fAQ7zY1EuCUei.Gbtbq7ZMm1Pn9etLVVOneeOhbu', '2021-10-13 14:09:30', 'member'),
 (49, 'mich-mich', 'jean-claude', 'mi******@***********.fr', 'michelle@groupomania.fr', '$2b$10$zuVdpTk/KgnOY1TYUikPy.L.UOHs1iZ7xfs.OYap6DZXz/Li5wqGC', '2021-10-13 23:56:41', 'member'),
 (50, 'druart', 'melodie', 'me**@***********.fr', 'melo@groupomania.fr', '$2b$10$w.BD9l1s0i1aFfO17f6h...EUTySAmEX37aIpXlLwKokk4uNzOimy', '2021-10-15 12:51:50', 'member'),
-(51, 'bastos', 'mickael', 'mi*****@***********.fr', 'mickael@groupomania.fr', '$2b$10$oMbfoqRzAzd/tzRP4iiFoOmBhfEDbhrJVj1TzAqjl7e.xyqktLXre', '2021-10-15 12:55:47', 'member');
+(51, 'bastos', 'mickael', 'mi*****@***********.fr', 'mickael@groupomania.fr', '$2b$10$oMbfoqRzAzd/tzRP4iiFoOmBhfEDbhrJVj1TzAqjl7e.xyqktLXre', '2021-10-15 12:55:47', 'member'),
+(52, 'toto', 'tutu', 'tu****@***********.fr', 'tutotu@groupomania.fr', '$2b$10$ONxz/QKVLlxA8T.Nj/np2OmcVIDHpVSk5QMeQ4NMV2w3W5.eoail2', '2021-11-03 14:18:22', 'member'),
+(53, 'tata', 'tata', 'ta**@***********.fr', 'tata@groupomania.fr', '$2b$10$2GN8qyIR6qX5TJ6kIgNr7eYa/ckI7AQe.iAIY40M09Ri6bNsWAr5m', '2021-11-03 14:21:31', 'member'),
+(55, 'Administrateur', 'Communauté', 'ad***@***********.fr', 'admin@groupomania.fr', '$2b$10$hLmq5.x8rBynCN2/AxmG..ijYG/Mdf/iWWn/MN05QfUE7jGm1UEQS', '2021-11-03 15:31:51', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -157,6 +147,14 @@ ALTER TABLE `comment`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_user_comment_id` (`user_id`),
   ADD KEY `fk_post_id` (`post_id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `post_id` (`post_id`);
 
 --
 -- Indexes for table `post`
@@ -182,16 +180,22 @@ ALTER TABLE `comment`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- Constraints for dumped tables
@@ -203,6 +207,13 @@ ALTER TABLE `user`
 ALTER TABLE `comment`
   ADD CONSTRAINT `fk_post_id` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `fk_user_comment_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `post`
